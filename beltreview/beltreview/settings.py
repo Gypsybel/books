237eb1d7b14ce53b219d31abab4ab8ed0a025a16
Gypsybel <crystabelrangel@gmail.com>
2016-08-18 11:27:13 -0700

    Update settings.py

--- beltreview/beltreview/settings.py
+++ beltreview/beltreview/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'beltreview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'books',
+        'USER': 'gypsybel'
+        'PASSWORD': 'clr86tink',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
